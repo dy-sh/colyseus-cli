e970a6c4aa75af9f26e8d459a94e66df19667635
derwish-pro <derwish.pro@gmail.com>
2017-07-30 22:51:41 +0300

    get param from server

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,14 @@ cli
     .option('-r, --route <url>', 'Route to connect (default: "/monitor")')
     .option('-s, --ssl', 'Use SSL (https instead of http)')
     .option('-v, --verbose', 'Verbose output')
+    .option('-g --get [param]',
+    `Get params from server. Available params:
+     registered_room - get all registered rooms names
+     room - get all created rooms
+     room/0 - get created room by id 
+     room/state/0 - get created room state by id
+     client - get all connected clients 
+    `)
     .version('0.1.0')
     .parse(process.argv);
 
@@ -21,9 +29,11 @@ let http = cli.ssl ? "https://" : "http://";
 let host = cli.address || "localhost";
 let port = cli.port || 2657;
 let route = cli.route || "/monitor";
-let url = http + host + ":" + port + route;
+let opearion = cli.get ? "/" + cli.get : "";
+let url = http + host + ":" + port + route + opearion;
 
-let waiting = true;
+
+let WAITING = true;
 
 if (cli.verbose)
     console.log("Connecting to " + url + " ...")
@@ -32,19 +42,32 @@ if (cli.verbose)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 request.get(url, (err, res, body) => {
-    if (err) {
+    if (err)
         console.log("Failed to connect to " + url + "\n" + err);
-    }
     else if (res.statusCode == 404)
-        console.log("Requested item is not found");
+        console.log("Requested item was not found");
     else {
-        console.log(body)
+        let info = JSON.parse(body);
+        if (opearion == "") {
+            console.log("Registered rooms: " + info.registeredRoomsCount);
+            console.log("Created rooms: " + info.createdRoomsCount)
+            console.log("Connected clients: " + info.connectedClientsCount)
+        }
+        // else if (opearion == "/registered_room") {
+        //     console.log("Registered rooms: " + info.registeredRoomsCount);
+        //     console.log("Created rooms: " + info.createdRoomsCount)
+        //     console.log("Connected clients: " + info.connectedClientsCount)
+        // }
+        else {
+
+            console.log(info)
+        }
     }
-    waiting = false;
+    WAITING = false;
 });
 
 
 
 
-function wait() { if (waiting) setTimeout(wait, 100); };
+function wait() { if (WAITING) setTimeout(wait, 100); };
 wait();
