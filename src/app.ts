a72c86f99c63b67d8e01f4b0fa55b68dfa042a3f
derwish-pro <derwish.pro@gmail.com>
2017-07-30 21:21:12 +0300

    route, verbose

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,28 @@
  */
 
 
-import * as program from 'commander');
+import * as cli from 'commander';
+import * as request from 'request';
 
-program
+cli
+    .option('-s, --server <address>', 'Sever address (default: "http://localhost")')
+    .option('-p, --port <number>', 'Port to connect (default: 2657)')
+    .option('-r, --route <url>', 'Route to connect (default: "/monitor")')
+    .option('-v, --verbose', 'Verbose output')
     .version('0.1.0')
-    .option('-h, --host <address>', 'Host address (default http://localhost)')
-    .option('-p, --port <number>', 'Port to connect (default 2657)')
     .parse(process.argv);
 
-let host = program.host || "http://localhost";
-let port = program.port || 2657;
-let url = host + ":" + port;
+let host = cli.host || "http://localhost";
+let port = cli.port || 2657;
+let route = cli.route || "/monitor";
+let url = host + ":" + port + route;
 
-console.log(url);
+if (cli.verbose)
+    console.log("Connecting to " + url)
+
+request(url, (err, res, body) => {
+    if (err) return "ERROR: " + err;
+
+    console.log(url);
+});
+
